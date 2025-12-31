aea5e35297648479caa1b07588a827ad71fa48e2
drfailer <drfailer.42@gmail.com>
2025-12-31 17:31:43 +0100

    debug build

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-odin build demo -define:FONT=/usr/share/fonts/adwaita-mono-fonts/AdwaitaMono-Regular.ttf
+odin build demo -define:FONT=/usr/share/fonts/adwaita-mono-fonts/AdwaitaMono-Regular.ttf -debug
